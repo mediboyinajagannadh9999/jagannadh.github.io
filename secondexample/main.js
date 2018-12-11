e62ca3d48e924579d3fce2e4b61fd0fcffaee9ee
mediboyinajagannadh9999 <45782295+mediboyinajagannadh9999@users.noreply.github.com>
2018-12-11 13:00:16 +0530

    add file via uplode

---
+++ secondexample/main.js
@@ -0,0 +1,203 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error('Cannot find module "' + req + '".');
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".date-as-calendar {\r\n    font-variant: normal;\r\n    font-style: normal;\r\n    font-weight: normal;\r\n    font-family: \"Helvetica\", \"Arial\", sans-serif;\r\n    vertical-align: top;\r\n  color: black;\r\n  background: white;\r\n  background : linear-gradient(to bottom right, #FFF 0%, #EEE 100%);\r\n\r\n  border: 1px solid #888;\r\n  border-radius: 3px;\r\n  overflow: hidden;\r\n\r\n  box-shadow: 2px 2px 2px -2px black;\r\n}\r\n.date-as-calendar .weekday,\r\n.date-as-calendar .day,\r\n.date-as-calendar .month,\r\n.date-as-calendar .year {\r\n  text-align: center;\r\n  line-height: 1.0;\r\n}\r\n.date-as-calendar .month {\r\n  font-family: \"Oswald\", sans-serif;\r\n  text-transform: uppercase;\r\n  background: #B11;\r\n  background : linear-gradient(to bottom right, #D66 0%, #A00 100%);\r\n  color: white;\r\n}"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n<form>\n\n<body>\n        <li style=\"text-align: left\">Business</li>\n        <li style=\"text-align: right\">9493092355</li>\n    <body style=\"background-color:aquamarine\">\n    <div class=\"container\">\n   \n        <div id=\"myCarousel\" class=\"carousel slide\" data-ride=\"carousel\">\n          <!-- Indicators -->\n          <ol class=\"carousel-indicators\">\n            <li data-target=\"#myCarousel\" data-slide-to=\"0\" class=\"active\"></li>\n            <li data-target=\"#myCarousel\" data-slide-to=\"1\"></li>\n            <li data-target=\"#myCarousel\" data-slide-to=\"2\"></li>\n          </ol>\n      \n          <!-- Wrapper for slides -->\n          <div class=\"carousel-inner\">\n            <div class=\"item active\">\n              <img src=\"../assets/images/p1.jpg\" alt=\"Los Angeles\" style=\"width:100%;height:50%;\">\n            </div>\n      \n            <div class=\"item\">\n              <img src=\"../assets/images/rose-blue-flower-rose-blooms-67636.jpeg\" alt=\"Chicago\" style=\"width:100%;height:50%;\">\n            </div>\n          \n            <div class=\"item\">\n              <img src=\"../assets/images/p3.jpeg\" alt=\"New york\" style=\"width:100%;height:50%;\">\n            </div>\n          </div>\n          <!-- Left and right controls -->\n          <a class=\"left carousel-control\" href=\"#myCarousel\" data-slide=\"prev\">\n            <span class=\"glyphicon glyphicon-chevron-left\"></span>\n            <span class=\"sr-only\">Previous</span>\n          </a>\n          <a class=\"right carousel-control\" href=\"#myCarousel\" data-slide=\"next\">\n            <span class=\"glyphicon glyphicon-chevron-right\"></span>\n            <span class=\"sr-only\">Next</span>\n          </a>\n        </div>\n      </div>\n    \n      <div>\n        <h1 style=\"text-align: center\"><b>Welcome to Business</b></h1>\n        <h2><i>\n          Business Know-How provides generalized information only. Be sure to consult with your own legal, tax or other advisers before taking any steps that could have an adverse effect on your business.\n        </i> </h2>\n      </div>\n      </body>\n      <body style=\"background-color: white\">\n   <div>\n     <h1><img src=\"../assets/images/rose-blue-flower-rose-blooms-67636.jpeg\" style=\"width:20%;\" align=\"left\"></h1>\n     <h1 style=\"text-align: right\"><b>About</b></h1>\n     <p><i>Business is the activity of making one's living or making money by producing or buying and selling products (such as goods and services).[1][2][need quotation to verify][3][4] Simply put, \n     it is \"any activity or enterprise entered into for profit. It does not mean it is a company, a corporation, partnership, or have any such formal organization, but it can range from a street peddler to General Motors.\"[5]</i></p>\n\n   </div> \n   </body>\n   <br><br><br>\n   <body>\n    <div class=\"container\">\n      <img src=\"../assets/images/11.png\" alt=\"Notebook\" style=\"width:10%;\">\n      <div class=\"content\">\n        <div align=\"left\">\n        <h1>Heading</h1>\n        <p>Lorem ipsum dolor sit amet, an his etiam torquatos.</p>\n        <button type=\"submit\">Click</button>\n        </div>\n        <div align=\"center\">\n      <img src=\"../assets/images/22.png\" alt=\"Notebook\" style=\"width:10%;\">\n        <h1>Heading</h1>\n        <p>Lorem ipsum dolor sit amet, an his etiam torquatos.</p>\n        <button type=\"submit\">Click</button>\n        </div>\n        <div align=\"right\">\n            <img src=\"../assets/images/seef.png\" alt=\"Notebook\" style=\"width:10%;\">\n              <h1>Heading</h1>\n              <p>Lorem ipsum dolor sit amet, an his etiam torquatos.</p>\n              <button type=\"submit\">Click</button>\n              </div>\n      </div>\n    </div>\n   </body>\n\n<form>\n  <body style=\"background-color: aquamarine\">\n  <div style=\"text-align: center\">\n  <b style=\"color:lightslategray;\">\n  SINGUP</b></div>\n  <fieldset>\n<div class=\"row\">\n            <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\" >\n\n            </div>\n            <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n               <b> FullName: </b>\n            </div>\n            <div class=\"col-lg-3 col-md-4 col-sm-3 col-xs-1\">\n                    <input type=\"text\" placeholder=\"Enter name\" name=\"name\" required>\n            </div>\n            <div class=\"col-lg-3 col-md-5 col-sm-3 col-xs-1\">\n\n            </div>\n </div>\n\n\n<div class=\"row\">\n            <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n\n            </div>\n            <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n             <b>Email:</b>\n            </div>\n            <div class=\"col-lg-4 col-md-4 col-sm-3 col-xs-1\">\n            <input type=\"email\" placeholder=\"Enter email\" name=\"email\" required>\n            </div>\n            <div class=\"col-lg-2 col-md-5 col-sm-3 col-xs-1\">\n\n           </div>\n</div>\n<div class=\"row\">\n    <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n\n      </div>\n      <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n       <b>Course:</b>\n      </div>\n        <div class=\"col-lg-4 col-md-4 col-sm-3 col-xs-1\">\n                <select name=\"course\">\n                        <option value=\"B.Arch\">B.Arch</option>\n                        <option value=\"B.A\">B.A</option>\n                        <option value=\"B.Com\">B.Com</option>\n                        <option value=\"B.Tech\">B.Tech</option>\n                      </select>\n        </div>\n        <div class=\"col-lg-2 col-md-4 col-sm-3 col-xs-1\">\n\n        </div>\n</div>\n\n<div class=\"row\">\n  <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n\n  </div>\n  <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n      <b>Date of Birth:</b>\n  </div>\n  <div class=\"col-lg-4 col-md-4 col-sm-3 col-xs-1\">\n      <input type=\"date\" required>\n  </div>\n  <div class=\"col-lg-2 col-md-4 col-sm-3 col-xs-1\">\n\n    </div>\n\n</div>\n\n\n<div class=\"row\">\n    <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n\n      </div>\n      <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n       <b>Gender:</b>\n      </div>\n        <div class=\"col-lg-4 col-md-4 col-sm-3 col-xs-1\">\n        <form  required>\n            <input type=\"radio\" name=\"gender\" value=\"male\"><i>Male</i> &emsp; &emsp;\n            <input type=\"radio\" name=\"gender\" value=\"female\"><i>Female</i> &emsp; &emsp;\n            <input type=\"radio\" name=\"gender\" value=\"other\"><i>Other</i> &emsp; &emsp;\n          </form>\n          </div>\n        <div class=\"col-lg-5 col-md-4 col-sm-3 col-xs-1\">\n\n        </div>\n</div>\n\n<div class=\"row\">\n    <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n\n      </div>\n      <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n       <b>Language:</b>\n      </div>\n  <div class=\"col-lg-4 col-md-4 col-sm-3 col-xs-1\">\n    <form >\n        <input type=\"checkbox\" name=\"language\" value=\"Telugu\"> Telugu\n        <input type=\"checkbox\" name=\"language\" value=\"Hindi\"> Hindi\n        <input type=\"checkbox\" name=\"language\" value=\"English\">English\n    </form>\n  </div>\n  <div class=\"col-lg-5 col-md-4 col-sm-3 col-xs-1\">\n\n</div>\n</div>\n<div class=\"row\">\n    <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n\n      </div>\n      <div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-1\">\n       <b>Adderss:</b>\n      </div>\n<div  class=\"col-lg-4 col-md-4 col-sm-3 col-xs-1\">\n  <textarea rows=\"4\" cols=\"20\" name=\"Address\" form=\"usrform\">Enter your Adderss</textarea>\n</div>\n<div class=\"col-lg-2 col-md-4 col-sm-3 col-xs-1\">\n\n  </div>\n  </div>\n  <div class=\"row\">\n      <div class=\"col-lg-5 col-md-5 col-sm-3 col-xs-1\">\n\n        </div>\n  <div class=\"col-lg-2 col-md-2 col-sm-6 col-xs-10\">\n  <button type=\"reset\" class=\"btn btn-warning\" ><i>Cancel</i></button> &emsp;\n  \n  <button type=\"submit\" class=\"btn btn-primary\"><i>Save</i></button>\n  \n  </div>\n  <div class=\"col-lg-5 col-md-5 col-sm-3 col-xs-1\">\n\n    </div>\n    \n  </div>\n  \n  \n  </fieldset>\n  \n</body>\n\n    </form>\n    \n</body>\n</form>\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: APPComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "APPComponent", function() { return APPComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var APPComponent = /** @class */ (function () {
+    function APPComponent() {
+        this.showimg = true;
+    }
+    APPComponent.prototype.register = function (data) {
+        console.log(data);
+    };
+    APPComponent.prototype.cancel = function () {
+        console.log("cencelled");
+    };
+    APPComponent.prototype.toggleDetails = function () {
+        this.showimg = !this.showimg;
+    };
+    APPComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], APPComponent);
+    return APPComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_2__["APPComponent"],
+            ],
+            imports: [
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["APPComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! E:\programs\secondexample\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
